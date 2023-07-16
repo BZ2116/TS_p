4f7ca85c1dfd1a4d68d05e5ee366e605220f15d7
BZ2116 <116424952+BZ2116@users.noreply.github.com>
2023-07-16 10:49:36 +0800

    DeepReadonly.ts

--- DeepReadonly.ts
+++ DeepReadonly.ts
@@ -1,6 +1,8 @@
 type DeepReadonly<T> = {
   readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
 };
+//[K in keyof T]遍历T中元素
+//判断T[K]是否为对象，如果是再将其类型定为DeepReadonly，形成递归结构，如果不是就直接为本身
 
 type X = {
   x: {
